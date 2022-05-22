e0530b22d3b565b40a32bcaae6233f807ebc8d8a
MarceloJSCesar <jsdevmarcelo@gmail.com>
2022-05-22 07:15:30 -0100

    added toMap() and toString() function

--- devnology_testapp/lib/app/models/cart.dart
+++ devnology_testapp/lib/app/models/cart.dart
@@ -1,6 +1,6 @@
 class Cart {
   final String itemImg;
-  final int itemQuatity;
+  final int itemQuantity;
   final String itemLabel;
   final double itemPrice;
 
@@ -8,6 +8,19 @@ class Cart {
     required this.itemImg,
     required this.itemLabel,
     required this.itemPrice,
-    required this.itemQuatity,
+    required this.itemQuantity,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'itemImg': itemImg,
+      'itemLabel': itemLabel,
+      'itemPrice': itemPrice,
+      'itemQuatity': itemQuantity,
+    };
+  }
+
+  @override
+  String toString() =>
+      'cart:[itemImg: $itemImg, itemLabel: $itemLabel, itemPrice: $itemPrice, itemQuantity: $itemQuantity]';
 }
