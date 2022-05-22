0267437639af7aa0b9c19c513d3920abb1766317
MarceloJSCesar <jsdevmarcelo@gmail.com>
2022-05-22 07:42:18 -0100

    app_dbnames config file created

---
+++ devnology_testapp/lib/app/config/app_dbnames.dart
@@ -0,0 +1,9 @@
+class AppDbNames {
+  // storage table
+  static String storageTable = 'storageTable';
+  // storage table columns
+  static String itemImg = 'itemImgColumn';
+  static String itemLabel = 'itemLabelColumn';
+  static String itemPrice = 'itemPriceColumn';
+  static String itemQuantity = 'itemQuantityColumn';
+}
---
+++ devnology_testapp/lib/app/database/db.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class Db {
+  static final Db _instance = Db.internal();
+  factory Db() => _instance;
+
+  Database? _db;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db as Database;
+    } else {
+      _db = await initDb();
+      return _db as Database;
+    }
+  }
+
+  Db.internal();
+
+  Future<Database> initDb() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, 'dbStorage.db');
+
+    return await openDatabase(path, version: 1,
+        onCreate: (database, newerVersion) async {
+      await database.execute(
+        'CREATE TABLE ${AppDbNames.storageTable}(${AppDbNames.id} INTEGER PRIMARY KEY, ${AppDbNames.email} TEXT, ${AppDbNames.password} TEXT, ${AppDbNames.name} TEXT, ${AppDbNames.school} TEXT, ${AppDbNames.sexuality} TEXT)',
+      );
+    });
+  }
+}
+}
